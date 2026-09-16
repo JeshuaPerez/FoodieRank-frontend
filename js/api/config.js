6eec67ea61b793cc777c2231c07ffcdcefc68ae0
juanlema-16 <juanlema160703@gmail.com>
2026-09-16 12:05:15 -0600

    chore(config): apuntar API_BASE_URL al backend desplegado en Render

--- js/api/config.js
+++ js/api/config.js
@@ -1,1 +1,3 @@
-const API_BASE_URL = 'http://localhost:3000/api';
+git add js/api/config.js
+git commit -m "chore(config): apuntar API_BASE_URL al backend desplegado en Render"
+git push -u origin feature/configuracion-produccion
